351f002b5a55ce247cbfd820daa5daffaec8d280
one10506 <one10506@gmail.com>
2017-06-06 22:23:42 +0800

    Update views.py

--- FinalProject/map/views.py
+++ FinalProject/map/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from .models import Photo
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def photo_list(request):
+	queryset = Photo.objects.all()
+	context = {
+		"photos": queryset,
+	}
+	return render(request, "map/photo.html", context)
