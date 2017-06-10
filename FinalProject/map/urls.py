46ddd4a28eb19a6c9c410c2aff61c711deb06c60
one10506 <one10506@gmail.com>
2017-06-11 03:10:01 +0800

    Create urls.py

--- FinalProject/map/urls.py
+++ FinalProject/map/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.photo_list, name='photo_list'),
+    url(r'^record/$', views.record, name='record'),
+    url(r'^diary/$',views.diary, name='diary'),
 ]
