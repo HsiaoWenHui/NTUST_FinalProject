302025361ff7e661cc1cc8b45bdb7b184d3f5531
one10506 <one10506@gmail.com>
2017-06-13 00:28:22 +0800

    Create views.py

--- FinalProject/map/views.py
+++ FinalProject/map/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Photo
+from .models import Photo,Map
 
 
 
@@ -12,7 +12,11 @@ def photo_list(request):
 	return render(request, "map/photo.html", context)
 
 def record(request):
-	return  render(request, "map/record.html")
+	queryset = Map.objects.all()
+	context = {
+		"maps":queryset,
+	}
+	return  render(request, "map/record.html",context)
 
 def diary(request):
         return  render(request, "map/diary.html")
