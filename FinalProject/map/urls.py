6df0070a88b8536b5a2f7d2501418921602e3e6e
one10506 <one10506@gmail.com>
2017-06-15 02:41:24 +0800

    Create urls.py

--- FinalProject/map/urls.py
+++ FinalProject/map/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.photo_list, name='photo_list'),
     url(r'^record/$', views.record, name='record'),
     url(r'^diary/$',views.diary, name='diary'),
+    url(r'^Diary_Page/$',views.Diary_Page, name='Diary_Page'),
 ]
